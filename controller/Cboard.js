72f30eb5ccea7af93e80829d4ee4a39859e65863
김태균 <rlaxorbs159@gmail.com>
2023-09-09 01:06:37 +0900

    refactor: board 라우터로 이동 코드 추가
    description: 라우터 분리를 위해서 app.js 에서 /board 경로의 진입을  board라우터로 이동시키는 코드 추가

--- controller/Cboard.js
+++ controller/Cboard.js
@@ -1,2 +1,25 @@
-const { Board } = require('../models/Board');
+const { Board } = require('../models/index');
 const { Op } = require('sequelize');
+
+exports.getBoardList = async (req, res) => {
+  try {
+    const BoardList = await Board.findAll();
+    res.send(BoardList);
+  } catch (error) {
+    console.error(error);
+    res.send('Internal Server Error');
+  }
+};
+
+exports.getBoard = async (req, res) => {
+  try {
+    const { bId } = req.params;
+    const board = await Board.findOne({
+      where: { bId: bId },
+    });
+    res.send(board);
+  } catch (error) {
+    console.error(error);
+    res.send(error);
+  }
+};
