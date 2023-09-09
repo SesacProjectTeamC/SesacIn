21dcdfbddb618ff95cb3232fc6aa89cfcf9b1276
정채림 <chaelimj45@gmail.com>
2023-09-09 15:21:40 +0900

    "feat: login.ejs 완성"
    description: form 완성 css적용 완료

--- controller/Cpage.js
+++ controller/Cpage.js
@@ -9,3 +9,11 @@ exports.login = (req, res) => {
     title: "test",
   });
 };
+
+exports.login = (req, res) => {
+  res.render("login",{
+    title: "test",
+    uId: req.body, 
+    pw: req.body ,
+  })
+}
