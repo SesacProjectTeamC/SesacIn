9b60c022f346d4e1df57ebb81a099051d3acf4bb
Sangwo0 <aa6618@naver.com>
2023-09-10 22:05:20 +0900

    refactor: 모달 추가 및 카드 수정

--- static/js/questionCreate.js
+++ static/js/questionCreate.js
@@ -23,15 +23,34 @@ const postBoard = () => {
     const title = document.querySelector("#title");
     const content = editor.getData();
     const type = "front";
+    if (title.value === "" || !title.value) {
+        appendAlert("제목을 입력해 주세요");
+    } else if (content === "" || !content) {
+        appendAlert("내용을 입력해 주세요");
+    } else {
+        axios({
+            method: "POST",
+            url: "/question/create",
+            data: { title: title.value, content: content, qType: type },
+        }).then((res) => {
+            if (res) {
+                console.log(res.data.result.qId);
+                document.location.href = `/question/${res.data.result.qId}`;
+            }
+        });
+    }
+};
 
-    axios({
-        method: "POST",
-        url: "/question/create",
-        data: { title: title.value, content: content, qType: type },
-    }).then((res) => {
-        if (res) {
-            console.log(res.data.result.qId);
-            document.location.href = `/question/${res.data.result.qId}`;
-        }
-    });
+const alertPlaceholder = document.querySelector("#alertC");
+const appendAlert = (message) => {
+    const wrapper = document.createElement("div");
+    wrapper.innerHTML = [
+        `<div style="display: none" id="myAlert" class="alert alert-danger alert-dismissible fade show" role="alert">`,
+        `   <div>${message}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        "</div>",
+    ].join("");
+
+    alertPlaceholder.append(wrapper);
+    $("#myAlert").fadeIn();
 };
