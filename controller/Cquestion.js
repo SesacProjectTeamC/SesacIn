76b80a4a0315bcc69a9a5570ac15a690368c76ac
saywhat <95282021+loveflora@users.noreply.github.com>
2023-09-10 14:20:08 +0900

    feat: 질문 게시판 삭제, 추가 기능

--- controller/Cquestion.js
+++ controller/Cquestion.js
@@ -11,87 +11,75 @@ exports.getQuestions = async (req, res) => {
   }
 };
 
-//-- 특정 질문만 가져오기 (답변 제외)
-// exports.getQuestion = async (req, res) => {
-//   try {
-//     const { qId } = req.params;
-
-//     const question = await Question.findOne({
-//       where: { qId },
-//     });
-
-//     res.render("question", { data: question });
-//     console.log("question>>>", question);
-//   } catch (err) {
-//     console.log(err);
-//     res.send("Internet Server Error!!!");
-//   }
-// };
-
-//-- 상우님이 원하시는 데이터
-// ("req.params { uId }");
-
-// res.body
-// {
-//   ""uId"": ""hjj"",
-//   ""pw"": ""1"",
-//   ""uName"": ""hj"",
-//   ""email"": ""jin@gmail.com"",
-//   ""isSesac"": true,
-//   ""campus"": ""영등포"",
-//   ""createdAt"": ""2023-09-08T07:55:46.000Z"",
-//   ""updatedAt"": ""2023-09-08T07:55:46.000Z""
-//   ""answerData"": {
-//       {
-//           uName: ""ff"",
-//           content: ""에라모르겠다""
-//           createdAt: ""2023-09-08T07:55:46.000Z"",
-//           likeCount: 5
-//       },
-//       {
-//           uName: ""ff"",
-//           content: ""에라모르겠다""
-//           createdAt: ""2023-09-08T07:55:46.000Z"",
-//           likeCount: 5
-//       },
-//    }
-// }
-
-// 질문 올리기
-exports.postQuestion = async (req, res) => {
+// 질문 생성 GET
+exports.getCreateQuestion = async (req, res) => {
   try {
-    const { qId, title, content, viewCount, likeCount, type, uId } = req.body;
-    const newQuestion = await Question.create({
-      qId,
-      title,
-      content,
-      viewCount,
-      likeCount,
-      type,
-      uId,
-    });
-    res.send(newQuestion);
+    res.render("questionCreateTest");
   } catch (err) {
     console.error(err);
     res.send("Internal Server Error");
   }
 };
 
-// 질문 수정하기
+// 질문 생성 POST
+exports.postQuestion = async (req, res) => {
+  // test login
+  req.session.user = 1;
+
+  if (!req.session.user) {
+    res.redirect("/");
+  }
+  let loginUser = req.session.user;
+
+  try {
+    const { title, content, qType } = req.body;
+    const newQuestion = await Question.create({
+      title,
+      content,
+      viewCount: 0,
+      likeCount: 0,
+      qType,
+      uId: loginUser,
+    });
+    console.log("추가>>>", { result: newQuestion });
+    res.send({ result: newQuestion });
+  } catch (err) {
+    console.error(err);
+    res.send("Internal Server Error");
+  }
+};
+
+// 질문 수정 GET
+exports.getEditQuestion = async (req, res) => {
+  try {
+    const { qId } = req.params;
+
+    // 특정 질문만 가져오기
+    const question = await Question.findOne({
+      where: { qId },
+    });
+
+    res.render("questionEditTest", { data: question });
+  } catch (err) {
+    console.error(err);
+    res.send("Internal Server Error");
+  }
+};
+
+// 질문 수정 PATCH
 exports.patchQuestion = async (req, res) => {
   try {
     const { qId } = req.params;
-    const { team_id } = req.body;
+    const { title, content } = req.body;
 
     const updatedQuestion = await Question.update(
-      { team_id: team_id },
+      { title, content },
       {
         where: { qId },
-        // params에 있는 Question_id를 Question_id로 바꾸겠다
       },
     );
 
-    res.send(updatedQuestion);
+    res.render("question", { result: updatedQuestion });
   } catch (err) {
     console.log(err);
     res.send("Internet Server Error!!!");
@@ -104,15 +92,15 @@ exports.deleteQuestion = async (req, res) => {
     const { qId } = req.params;
 
     const isDeleted = await Question.destroy({
-      where: { qId: qId },
+      where: { qId },
     });
 
     console.log("isDeleted >>>", isDeleted); // 성공 시 1, 실패 시 0
 
     if (isDeleted) {
-      return res.send(true);
+      return res.send({ result: true });
     } else {
-      return res.send(false);
+      return res.send({ result: false });
     }
   } catch (err) {
     console.log(err);
