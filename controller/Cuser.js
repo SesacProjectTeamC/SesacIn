b51aae9d3f6cc0c119977359c759991d4cc5db22
정채림 <chaelimj45@gmail.com>
2023-09-11 18:16:15 +0900

    "feat: code"

--- controller/Cuser.js
+++ controller/Cuser.js
@@ -1,19 +1,19 @@
-const { User } = require('../models');
-const { Op } = require('sequelize');
-const bcrypt = require('bcrypt');
+const {User} = require("../models");
+const {Op} = require("sequelize");
+const bcrypt = require("bcrypt");
 
 // 회원가입 창 렌더링
 exports.getJoin = (req, res) => {
-  res.render('join');
+  res.render("join");
 };
 
 // 회원 가입 시 사용자 생성
 exports.postUser = async (req, res) => {
   console.log(req.body);
   try {
-    let { uId, pw, uName, email, isSesac, campus } = req.body;
+    let {uId, pw, uName, email, isSesac, campus} = req.body;
     // db에 넣기전 pw 암호화
-    pw = hashPassword(pw);
+    // pw = hashPassword(pw);
 
     // null 값이거나 비속어 등이 섞여있으면 처리하기
 
@@ -28,7 +28,7 @@ exports.postUser = async (req, res) => {
     res.send(newUser);
   } catch (err) {
     console.error(err);
-    res.send('Internal Server Error');
+    res.send("Internal Server Error");
   }
 };
 
@@ -37,29 +37,29 @@ exports.postUser = async (req, res) => {
 /// 회원 조회
 exports.getUser = async (req, res) => {
   try {
-    const { uId } = req.params; // 객체에서 꺼내온 유저 아이디 값
+    const {uId} = req.params; // 객체에서 꺼내온 유저 아이디 값
 
     // 세션에서 로그인 된 사용자 id 가져오기
     const loginUserId = req.session.user;
-
+    console.log(loginUserId);
     // 현재 로그인 된 사용자의 ID와 요청된 사용자 ID가 일치하는지 확인
     if (loginUserId === uId) {
       // 데이터베이스에서 해당 사용자 정보를 조회합니다.
       const user = await User.findOne({
-        where: { uId: uId },
+        where: {uId: uId},
       });
 
       // 사용자 정보를 마이페이지 템플릿에 전달하여 렌더링합니다.
-      res.render('profile', { userData: user });
+      res.render("profile", {userData: user});
     } else {
       // 현재 로그인한 사용자와 요청된 사용자가 다를 경우 권한 없음을 응답
       res
         .status(403)
-        .send('Forbidden: You do not have permission to access this page.');
+        .send("Forbidden: You do not have permission to access this page.");
     }
   } catch (err) {
     console.error(err);
-    res.send('Internal Server Error');
+    res.send("Internal Server Error");
   }
 };
 
@@ -72,7 +72,7 @@ exports.getUserInfo = (req, res) => {
     uId: req.session.user, // 세션에서 사용자 ID 가져오기
   };
   // ********** 추후에 어떤 화면으로 이동할 지 이름 수정 필요할수도 있음
-  res.render('editprofile', { userData });
+  res.render("editprofile", {userData});
 };
 
 // 회원 정보 수정 - 비밀번호, 이름 (이미지는 후순위)
@@ -81,16 +81,16 @@ exports.patchUser = async (req, res) => {
     const uId = req.session.user;
     console.log(uId);
 
-    const userData = { uId: uId };
-    let { pw, uName } = req.body;
+    const userData = {uId: uId};
+    let {pw, uName} = req.body;
     console.log(req.body);
 
     pw = hashPassword(pw);
     // update는 바꿔야하는 인자, 어디에 있는 건지 where 인자
     const updatedUser = await User.update(
-      { pw: pw, uName: uName },
+      {pw: pw, uName: uName},
       {
-        where: { uId },
+        where: {uId},
       }
     );
 
@@ -98,7 +98,7 @@ exports.patchUser = async (req, res) => {
     // res.redirect('/') // 추후 어떤 페이지로 이동시키려면 수정
   } catch (err) {
     console.error(err);
-    res.send('Internal Server Error');
+    res.send("Internal Server Error");
   }
 };
 
@@ -107,7 +107,7 @@ exports.deleteUser = async (req, res) => {
   try {
     const uId = req.session.user;
     const isDeleted = await User.destroy({
-      where: { uId },
+      where: {uId},
     });
     // console.log(isDeleted); // 성공시 1, 실패시 0
     if (isDeleted) {
@@ -115,7 +115,7 @@ exports.deleteUser = async (req, res) => {
       req.session.destroy((err) => {
         if (err) {
           console.log(err);
-          return res.status(500).send('Internal Server Error');
+          return res.status(500).send("Internal Server Error");
         }
         res.send(true);
       });
@@ -125,14 +125,14 @@ exports.deleteUser = async (req, res) => {
     }
   } catch (err) {
     console.error(err);
-    res.send('Internal Server Error');
+    res.send("Internal Server Error");
   }
 };
 
 // 로그인 페이지 렌더링
 exports.login = (req, res) => {
-  res.render('login', {
-    title: 'test',
+  res.render("login", {
+    title: "test",
     uId: req.body,
     pw: req.body,
   });
@@ -148,7 +148,7 @@ exports.userLogin = async (req, res) => {
 
   // 아이디 검사
   if (resultUser === null) {
-    return res.status(401).json({ message: '사용자를 찾을 수 없습니다.' });
+    return res.status(401).json({message: "사용자를 찾을 수 없습니다."});
   }
 
   // 암호 검사
@@ -160,11 +160,11 @@ exports.userLogin = async (req, res) => {
     req.session.user = resultUser.uId;
     // 성공 응답 보내주기
     return res.status(200).json({
-      message: '로그인 성공',
+      message: "로그인 성공",
       isLogin: true,
     });
   } else {
-    return res.status(401).json({ message: '비밀번호 불일치' });
+    return res.status(401).json({message: "비밀번호 불일치"});
     // 비밀번호 불일치
   }
 };
@@ -176,7 +176,7 @@ exports.userLogout = (req, res) => {
       console.log(err);
       return;
     }
-    res.redirect('/');
+    res.redirect("/");
   });
 };
 
