9191fca263787e79ec9a3c8ebbba77ab6d34bec1
saywhat <95282021+loveflora@users.noreply.github.com>
2023-09-11 11:52:24 +0900

    feat: comment 테이블에 qId 외래키 추가
    description: 이외 주석 설명 추가

--- models/index.js
+++ models/index.js
@@ -22,27 +22,48 @@ const Board = require('./Board')(sequelize, Sequelize);
 const Comment = require('./Comment')(sequelize, Sequelize);
 const Question = require('./Question')(sequelize, Sequelize);
 
+//=== Relation 설정 ===
+// 전체 1:다 관계
+
+// 1. User - Question
+// 한명의 유저는 여러개의 질문 가능 / 질문 올린 유저 존재
 User.hasMany(Question, { foreignKey: 'uId', onDelete: 'CASCADE' });
 Question.belongsTo(User, { foreignKey: 'uId', onDelete: 'CASCADE' });
+
+// 2. User - Answer
 // 한명의 유저는 여러개의 답변 가능 / 답변 올린 유저 존재
 User.hasMany(Answer, { foreignKey: 'uId', onDelete: 'CASCADE' });
 Answer.belongsTo(User, { foreignKey: 'uId', onDelete: 'CASCADE' });
-// 하나의 질문에는 여러개의 답변 가능 / 답변은 질문에 속해있음
-Question.hasMany(Answer, { foreignKey: 'qId', onDelete: 'CASCADE' });
-Answer.belongsTo(Question, { foreignKey: 'qId', onDelete: 'CASCADE' });
+
+// 3. User - Board
 // 한명의 유저는 여러개 게시글 업로드 가능 / 게시글 올린 사람 존재
 User.hasMany(Board, { foreignKey: 'uId', onDelete: 'CASCADE' });
 Board.belongsTo(User, { foreignKey: 'uId', onDelete: 'CASCADE' });
+
+// 4. User - Comment
 // 한명의 유저는 여러개의 댓글 가능 / 댓글 올린 유저 존재
 User.hasMany(Comment, { foreignKey: 'uId', onDelete: 'CASCADE' });
 Comment.belongsTo(User, { foreignKey: 'uId', onDelete: 'CASCADE' });
+
+// 5. Question - Answer
+// 하나의 질문에는 여러개의 답변 가능 / 답변은 질문에 속해있음
+Question.hasMany(Answer, { foreignKey: 'qId', onDelete: 'CASCADE' });
+Answer.belongsTo(Question, { foreignKey: 'qId', onDelete: 'CASCADE' });
+
+// 6. Answer - Comment
+// 하나의 답변에는 여러개의 댓글 가능 / 댓글은 답변에 속해있음
+Answer.hasMany(Comment, { foreignKey: 'aId', onDelete: 'CASCADE' });
+Comment.belongsTo(Answer, { foreignKey: 'aId', onDelete: 'CASCADE' });
+
+// 7. Question - Comment
+// 하나의 질문에는 여러개의 댓글 가능 / 댓글은 질문에 속해있음
+Question.hasMany(Comment, { foreignKey: 'qId', onDelete: 'CASCADE' });
+Comment.belongsTo(Question, { foreignKey: 'qId', onDelete: 'CASCADE' });
+
+// 8. Board - Comment
 // 하나의 게시글에는 여러개의 댓글 가능 / 댓글은 게시글에 속해있음
 Board.hasMany(Comment, { foreignKey: 'bId', onDelete: 'CASCADE' });
 Comment.belongsTo(Board, { foreignKey: 'bId', onDelete: 'CASCADE' });
-
-// 하나의 게시글에는 여러개의 댓글 가능 / 댓글은 게시글에 속해있음
-Answer.hasMany(Comment, { foreignKey: 'aId', onDelete: 'CASCADE' });
-Comment.belongsTo(Answer, { foreignKey: 'aId', onDelete: 'CASCADE' });
 
 db.sequelize = sequelize; // DB연결정보를 가진 시퀄라이저
 db.Sequelize = Sequelize; // 시퀄라이저 모듈
