072c144a01ef0a768a9d72a13c2121a3d3fea4cc
김태균 <rlaxorbs159@gmail.com>
2023-09-14 16:52:07 +0900

    refactor: 모든 페이지 렌더링 컨트롤러에 세션 추가

--- routes/index.js
+++ routes/index.js
@@ -1,147 +1,35 @@
 // (시작) module
 // 경로 선언과 관련된 내용 기술
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const Cuser = require("../controller/Cuser");
-const Cquestion = require("../controller/Cquestion");
-const Cboard = require("../controller/Cboard");
-const Cprofile = require("../controller/Cprofile");
-const CmyPage = require("../controller/CmyPage");
+const Cuser = require('../controller/Cuser');
+const Cquestion = require('../controller/Cquestion');
+const Cboard = require('../controller/Cboard');
+const Cprofile = require('../controller/Cprofile');
+const CmyPage = require('../controller/CmyPage');
 
-// 메인 페이지 관련
-router.get("/", Cquestion.getQuestions);
-router.get("/myPage", CmyPage.getHistory);
+// 메인 페이지 렌더링
+router.get('/', Cquestion.getQuestions);
 
-// router.get('/users/:uId/profile', Cprofile.getUser);
+// 마이페이지 렌더링 (사용 안하는 것 같음)
+router.get('/myPage', CmyPage.getHistory);
 
-// 유저 관련
-/**
- * @swagger
- * tags:
- *   name: User
- *   description: 사용자 관리
- */
-
-///////////////////////////////////// 회원가입 페이지
 // 회원 가입 페이지 렌더링
-router.get("/join", Cuser.getJoin);
+router.get('/join', Cuser.getJoin);
 
-// 중복 확인
+// 중복 확인 처리 (아이디/패스워드)
 router.get('/checkDuplicate', Cuser.checkDuplicate);
 
-
-
-/**
- * @swagger
- * /users:
- *   post:
- *     summary: 회원 가입 처리
- *     tags:
- *       - User
- *     description: 회원 가입 처리
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               uId:
- *                 type: string
- *                 description: 사용자 아이디
- *               pw:
- *                 type: string
- *                 description: 비밀번호
- *               uName:
- *                 type: string
- *                 description: 닉네임
- *               email:
- *                 type: string
- *                 description: 이메일
- *               isSesac:
- *                 type: string
- *                 description: 새싹인 여부
- *               campus:
- *                 type: string
- *                 description: 소속 캠퍼스
- *
- *     responses:
- *       200:
- *         description: 성공적으로 사용자 추가되었을 때 응답
- *       500:
- *         description: 서버 에러
- */
-// 회원 가입 관련 api
-// post /user 요청이 오면 사용자 추가
-router.post("/users", Cuser.postUser);
-
-
-//////////////////////////////////////// 로그인 페이지
+// 사용자 회원가입 처리
+router.post('/users', Cuser.postUser);
 
 // 로그인 페이지 렌더링
-router.get("/login", Cuser.login);
+router.get('/login', Cuser.login);
 
 // 로그인 처리
-/**
- * @swagger
- * /login:
- *   post:
- *     summary: 사용자 로그인
- *     description: 로그인 처리 진행
- *     tags:
- *       - User
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               uId:
- *                 type: string
- *                 description: 사용자 아이디
- *               pw:
- *                 type: string
- *                 description: 비밀번호
- *     responses:
- *       '200':
- *         description: 로그인 성공
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 isLogin:
- *                   type: boolean
- *                   description: 로그인 여부
- *       '401':
- *         description: 로그인 실패
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   description: 오류 메시지
- */
-router.post("/login", Cuser.userLogin);
+router.post('/login', Cuser.userLogin);
 
 // 로그아웃 처리
-/**
- * @swagger
- * /logout:
- *   post:
- *     summary: 사용자 로그아웃
- *     description: 로그인이 되어있다고 가정하고 session에 저장된 유저를 삭제하는 처리
- *     tags:
- *       - User
- *     responses:
- *       '302':
- *         description: 로그아웃 성공 및 리다이렉트
- *       '500':
- *         description: 서버 오류 발생
- */
-router.post("/logout", Cuser.userLogout);
+router.post('/logout', Cuser.userLogout);
 
 module.exports = router;
