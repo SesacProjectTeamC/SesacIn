43b2bea71f5ccb311eea42e821800a8431938083
hotdog7778 <rlaxorbs159@gmail.com>
2023-09-16 04:06:16 +0900

    refactor: login 여부 확인 및 login 페이지로 이동시키는 미들웨어 등록
    description :
     - /util/middleware
     - 해당 미들웨어가 등록된 라우터는 로그인 되어있지 않은 경우 로그인 페이지로 리다이렉트 시킵니다.
     - 따라서 로그인이 필수되어지는 라우터에 적용시켰습니다.

--- routes/questionRouter.js
+++ routes/questionRouter.js
@@ -3,10 +3,13 @@ const router = express.Router();
 const Cquestion = require('../controller/Cquestion');
 const Canswer = require('../controller/Canswer');
 const Ccomment = require('../controller/Ccomment');
+const { needToLogin } = require('../util/middleware');
 
-// 질문 추가
-router.get('/create', Cquestion.getCreateQuestion);
-router.post('/create', Cquestion.postQuestion);
+// 질문 추가 페이지 렌더링
+router.get('/create', needToLogin, Cquestion.getCreateQuestion);
+
+// 질문 추가 처리
+router.post('/create', needToLogin, Cquestion.postQuestion);
 
 // 특정 질문 페이지 렌더링
 router.get('/:qId', Cquestion.getQuestion);
@@ -25,13 +28,15 @@ router.patch('/:qId/like/:aId', Canswer.likeAnswer);
 // 페이지별 질문 조회
 router.get('/list/:page&:pageSize', Cquestion.paginateQuestion);
 
-// 특정 질문 내용 수정
-router.get('/:qId/edit', Cquestion.getEditQuestion);
+// 특정 질문 내용 수정 페이지 렌더링
+router.get('/:qId/edit', needToLogin, Cquestion.getEditQuestion);
+
+// 특정 질문 내용 수정 처리
 router.patch('/:qId/edit', Cquestion.patchQuestion);
 
 // 특정 질문 삭제
 // /question/${qId}/delete
-router.delete('/:qId/delete', Cquestion.deleteQuestion);
+router.delete('/:qId/delete', needToLogin, Cquestion.deleteQuestion);
 
 //==== Answer ====
 // 답변 전체 조회
@@ -42,11 +47,11 @@ router.get('/:qId/answer/create', Canswer.getCreateAnswer);
 router.post('/:qId/answer/create', Canswer.postAnswer);
 
 // 특정 답변 내용 수정
-router.get('/:qId/answer/:aId/edit', Canswer.getEditAnswer);
-router.patch('/:qId/answer/:aId/edit', Canswer.patchAnswer);
+router.get('/:qId/answer/:aId/edit', needToLogin, Canswer.getEditAnswer);
+router.patch('/:qId/answer/:aId/edit', needToLogin, Canswer.patchAnswer);
 
 // 특정 답변 삭제
-router.delete('/:qId/answer/:aId/delete', Canswer.deleteAnswer);
+router.delete('/:qId/answer/:aId/delete', needToLogin, Canswer.deleteAnswer);
 
 //=== Comment ===
 // 특정 질문의 특정 답변에 대한 댓글
@@ -63,6 +68,10 @@ router.get('/:qId/:aId/comment/:cId/edit', Ccomment.getEditComment);
 router.patch('/:qId/:aId/comment/:cId/edit', Ccomment.patchComment);
 
 // 삭제
-router.delete('/:qId/:aId/comment/:cId/delete', Ccomment.deleteComment);
+router.delete(
+  '/:qId/:aId/comment/:cId/delete',
+  needToLogin,
+  Ccomment.deleteComment
+);
 
 module.exports = router;
