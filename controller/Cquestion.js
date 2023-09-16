c8bc64a44226868f84a95983f1c3cc595ea916c1
hotdog7778 <rlaxorbs159@gmail.com>
2023-09-16 18:35:52 +0900

    refactor: board 좋아요 기능
    description:
     - 로그인시에만 좋아요 가능
     - 로그아웃된 상태에서 좋아요 시도하면 프론트로 401 보냄
       -> 로그인여부 검사하는 미들웨어에서 바로 리다이렉트 처리가 안돼서 프론트로 보냈습니다.

--- controller/Cquestion.js
+++ controller/Cquestion.js
@@ -1,5 +1,5 @@
-const { Question, Answer, Comment, uLike } = require("../models");
-const moment = require("moment");
+const { Question, Answer, Comment, uLike } = require('../models');
+const moment = require('moment');
 
 //=== 메인페이지,질문 목록 가져오기 ===
 exports.getQuestions = async (req, res) => {
@@ -19,7 +19,7 @@ exports.getQuestions = async (req, res) => {
 
     // 페이지별 Question호출
     const paginatedQuestions = await Question.findAll({
-      order: [["createdAt", "DESC"]], // 정렬 기준
+      order: [['createdAt', 'DESC']], // 정렬 기준
       limit: pageSize,
       offset: (page - 1) * pageSize,
     });
@@ -28,24 +28,24 @@ exports.getQuestions = async (req, res) => {
     const create = [];
 
     for (q of paginatedQuestions) {
-      create.push(moment(q.createdAt).format("YYYY-MM-DD"));
+      create.push(moment(q.createdAt).format('YYYY-MM-DD'));
     }
 
     if (isLogin) {
-      console.log("로그인O 사용자 >>>", req.session.user);
+      console.log('로그인O 사용자 >>>', req.session.user);
 
-      res.status(200).render("main", {
-        type: "qna",
+      res.status(200).render('main', {
+        type: 'qna',
         data: paginatedQuestions,
         pageCount: pageCount,
         cDate: create,
         isLogin,
       });
     } else {
-      console.log("로그인X");
+      console.log('로그인X');
 
-      res.render("main", {
-        type: "qna",
+      res.render('main', {
+        type: 'qna',
         data: paginatedQuestions,
         pageCount: pageCount,
         cDate: create,
@@ -54,7 +54,7 @@ exports.getQuestions = async (req, res) => {
     }
   } catch (err) {
     console.log(err);
-    res.send("Internet Server Error!!!");
+    res.send('Internet Server Error!!!');
   }
 };
 
@@ -73,7 +73,7 @@ exports.paginateQuestion = async (req, res) => {
     // 페이지에 해당하는 Question 데이터 조회
     const paginatedQuestions = await Question.findAll({
       //최신글 정렬
-      order: [["createdAt", "DESC"]],
+      order: [['createdAt', 'DESC']],
       limit: pageSize,
       offset: (page - 1) * pageSize,
     });
@@ -81,20 +81,20 @@ exports.paginateQuestion = async (req, res) => {
     // 날짜 데이터 포맷 변경
     const create = [];
     for (q of paginatedQuestions) {
-      create.push(moment(q.dataValues.createdAt).format("YYYY-MM-DD"));
+      create.push(moment(q.dataValues.createdAt).format('YYYY-MM-DD'));
     }
     console.log(paginatedQuestions[0]);
     res.send({
       questions: paginatedQuestions,
       pageCount,
       cDate: create,
-      msg: "페이지별 Question 호출 처리 완료",
+      msg: '페이지별 Question 호출 처리 완료',
     });
   } catch (error) {
     console.error(error);
     res.status(500).send({
       success: false,
-      error: "서버 에러",
+      error: '서버 에러',
     });
   }
 };
@@ -152,7 +152,7 @@ exports.getQuestion = async (req, res) => {
       uLikeAnswersResult.push(uLikeAnswerFindResult);
     }
 
-    return res.render("questionTest", {
+    return res.render('questionTest', {
       data: question,
       answerData: answers,
       commentData: comments,
@@ -164,7 +164,7 @@ exports.getQuestion = async (req, res) => {
     });
   } catch (err) {
     console.log(err);
-    res.send("Internet Server Error!!!");
+    res.send('Internet Server Error!!!');
   }
 };
 
@@ -181,13 +181,13 @@ exports.viewQuestion = async (req, res) => {
       { viewCount: question.viewCount + 1 },
       {
         where: { qId },
-      },
+      }
     );
 
     res.send({ data: question });
   } catch (err) {
     console.log(err);
-    res.send("Internet Server Error!!!");
+    res.send('Internet Server Error!!!');
   }
 };
 
@@ -199,14 +199,14 @@ exports.getCreateQuestion = async (req, res) => {
   try {
     if (!isLogin) {
       // 로그인 안한상태에서 QnA 글쓰기 페이지를 요청하면 로그인 페이지로 리다이렉트
-      res.status(301).redirect("/login");
+      res.status(301).redirect('/login');
     } else {
       // 로그인 되어있을때 페이지 렌더링
-      res.status(200).render("post", {
+      res.status(200).render('post', {
         isLogin,
         currentUser: req.session.user,
         data: {
-          type: "qna",
+          type: 'qna',
         },
       });
     }
@@ -218,10 +218,8 @@ exports.getCreateQuestion = async (req, res) => {
 
 //=== 질문 생성 POST ===
 exports.postQuestion = async (req, res) => {
-  if (!req.session.user) {
-    res.redirect("/");
-  }
-  let loginUser = req.session.user;
+  // 세션 확인
+  let isLogin = req.session.user ? true : false;
 
   try {
     const { title, content, qType } = req.body;
@@ -231,13 +229,13 @@ exports.postQuestion = async (req, res) => {
       viewCount: 0,
       likeCount: 0,
       qType,
-      uId: loginUser,
+      uId: req.session.user,
     });
-    console.log("추가>>>", { result: newQuestion });
+    console.log('추가>>>', { result: newQuestion });
     res.send({ result: newQuestion });
   } catch (err) {
     console.error(err);
-    res.send("Internal Server Error");
+    res.send('Internal Server Error');
   }
 };
 
@@ -253,7 +251,7 @@ exports.getEditQuestion = async (req, res) => {
       where: { qId },
     });
 
-    res.status(200).render("questionEditTest", {
+    res.status(200).render('questionEditTest', {
       data: question,
       isLogin,
       success: true,
@@ -264,7 +262,7 @@ exports.getEditQuestion = async (req, res) => {
     res.status(500).send({
       isLogin,
       success: false,
-      msg: "Internal Server Error",
+      msg: 'Internal Server Error',
     });
   }
 };
@@ -287,7 +285,7 @@ exports.patchQuestion = async (req, res) => {
       res.status(401).send({
         success: false,
         currentLoginUser: req.session.user,
-        msg: "게시글의 소유자가 아님",
+        msg: '게시글의 소유자가 아님',
       });
       return;
     }
@@ -296,7 +294,7 @@ exports.patchQuestion = async (req, res) => {
       { title, content },
       {
         where: { qId },
-      },
+      }
     );
 
     //%%% 타입 변경, 수정날짜에 대한 로직 추가 ?
@@ -304,7 +302,7 @@ exports.patchQuestion = async (req, res) => {
     res.send({ data: updatedQuestion });
   } catch (err) {
     console.log(err);
-    res.send("Internet Server Error!!!");
+    res.send('Internet Server Error!!!');
   }
 };
 
@@ -327,13 +325,13 @@ exports.deleteQuestion = async (req, res) => {
         result: false,
         isLogin,
         currentUser: req.session.user,
-        msg: "질문 게시글이 삭제되지 않았습니다.",
+        msg: '질문 게시글이 삭제되지 않았습니다.',
       });
       return;
     }
 
     // 정상 삭제 처리
-    res.redirect("/");
+    res.redirect('/');
   } catch (err) {
     console.log(err);
 
@@ -374,10 +372,7 @@ exports.likeQuestion = async (req, res) => {
       });
 
       // (2) 질문 likeCount 업데이트 +1
-      await Question.update(
-        { likeCount: getQuestion.likeCount + 1 },
-        { where: { qId } },
-      );
+      await Question.update({ likeCount: getQuestion.likeCount + 1 }, { where: { qId } });
     } else if (uLikeFind) {
       // 2) uLike findOne -> qId 있으면,
       // (1) 좋아요 -> uLike 해당 qId 삭제함
@@ -386,15 +381,12 @@ exports.likeQuestion = async (req, res) => {
       });
 
       // (2) 질문 likeCount 업데이트 -1
-      await Question.update(
-        { likeCount: getQuestion.likeCount - 1 },
-        { where: { qId } },
-      );
+      await Question.update({ likeCount: getQuestion.likeCount - 1 }, { where: { qId } });
     }
 
     res.send({ data: getQuestion, qResult: resultLike });
   } catch (err) {
     console.log(err);
-    res.send("Internet Server Error!!!");
+    res.send('Internet Server Error!!!');
   }
 };
