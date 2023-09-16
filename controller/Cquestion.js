daa61c31fb6f9e84cf16fa7b2196e0cc01d06eac
Sangwo0 <aa6618@naver.com>
2023-09-16 22:54:31 +0900

    fix: 충돌해결

--- controller/Cquestion.js
+++ controller/Cquestion.js
@@ -1,6 +1,120 @@
 const { Question, Answer, Comment, uLike } = require('../models');
 const moment = require('moment');
 
+//=== 메인페이지,질문 목록 가져오기 ===
+
+exports.getQuestions = async (req, res) => {
+  // 세션 검사
+  let isLogin = req.session.user ? true : false;
+
+  try {
+    // 한페이지 만큼 데이터 조회
+    let page = parseInt(req.params.page) || 1;
+    let pageSize = parseInt(req.params.pageSize) || 20;
+
+    // 전체 Question목록 개수 계산
+    const totalPage = await Question.count();
+
+    // 페이지 수 (올림처리)
+    const pageCount = parseInt(Math.ceil(totalPage / pageSize));
+
+    // 페이지별 Question호출
+    const paginatedQuestions = await Question.findAll({
+      order: [['createdAt', 'DESC']], // 정렬 기준
+      limit: pageSize,
+      offset: (page - 1) * pageSize,
+    });
+
+    // 날짜 데이터 포맷 변경
+    const create = [];
+
+    for (q of paginatedQuestions) {
+      create.push(moment(q.createdAt).format('YYYY-MM-DD'));
+    }
+
+    if (isLogin) {
+      console.log('로그인O 사용자 >>>', req.session.user);
+
+      res.status(200).render('main', {
+        type: 'qna',
+        data: paginatedQuestions,
+        pageCount: pageCount,
+        cDate: create,
+        isLogin,
+      });
+    } else {
+      console.log('로그인X');
+
+      res.render('main', {
+        type: 'qna',
+        data: paginatedQuestions,
+        pageCount: pageCount,
+        cDate: create,
+        isLogin,
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    res.send('Internet Server Error!!!');
+  }
+};
+
+exports.getQuestionsMain = async (req, res) => {
+  // 세션 검사
+  let isLogin = req.session.user ? true : false;
+
+  try {
+    // 한페이지 만큼 데이터 조회
+    let page = parseInt(req.params.page) || 1;
+    let pageSize = parseInt(req.params.pageSize) || 20;
+
+    // 전체 Question목록 개수 계산
+    const totalPage = await Question.count();
+
+    // 페이지 수 (올림처리)
+    const pageCount = parseInt(Math.ceil(totalPage / pageSize));
+
+    // 페이지별 Question호출
+    const paginatedQuestions = await Question.findAll({
+      order: [['createdAt', 'DESC']], // 정렬 기준
+      limit: pageSize,
+      offset: (page - 1) * pageSize,
+    });
+
+    // 날짜 데이터 포맷 변경
+    const create = [];
+
+    for (q of paginatedQuestions) {
+      create.push(moment(q.createdAt).format('YYYY-MM-DD'));
+    }
+
+    if (isLogin) {
+      console.log('로그인O 사용자 >>>', req.session.user);
+
+      res.status(200).render('listMain', {
+        type: 'qna',
+        data: paginatedQuestions,
+        pageCount: pageCount,
+        cDate: create,
+        isLogin,
+      });
+    } else {
+      console.log('로그인X');
+
+      res.render('listMain', {
+        type: 'qna',
+        data: paginatedQuestions,
+        pageCount: pageCount,
+        cDate: create,
+        isLogin,
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    res.send('Internet Server Error!!!');
+  }
+};
+
 //=== 질문 목록 가져오기(페이지별) ===
 // /question/list/:page&:pageSize
 exports.paginateQuestion = async (req, res) => {
@@ -92,6 +206,8 @@ exports.getQuestion = async (req, res) => {
         },
       });
 
+      console.log(':::::::::::', uLikeAnswersFind);
+
       // (2) 답변의 결과 (T/F)
       const uLikeAnswerFindResult = !!uLikeAnswersFind;
 
@@ -165,8 +281,10 @@ exports.getCreateQuestion = async (req, res) => {
 
 //=== 질문 생성 POST ===
 exports.postQuestion = async (req, res) => {
-  // 세션 확인
-  let isLogin = req.session.user ? true : false;
+  if (!req.session.user) {
+    res.redirect('/');
+  }
+  let loginUser = req.session.user;
 
   try {
     const { title, content, qType } = req.body;
@@ -320,7 +438,10 @@ exports.likeQuestion = async (req, res) => {
       });
 
       // (2) 질문 likeCount 업데이트 +1
-      await Question.update({ likeCount: getQuestion.likeCount + 1 }, { where: { qId } });
+      await Question.update(
+        { likeCount: getQuestion.likeCount + 1 },
+        { where: { qId } }
+      );
     } else if (uLikeFind) {
       // 2) uLike findOne -> qId 있으면,
       // (1) 좋아요 -> uLike 해당 qId 삭제함
@@ -329,7 +450,10 @@ exports.likeQuestion = async (req, res) => {
       });
 
       // (2) 질문 likeCount 업데이트 -1
-      await Question.update({ likeCount: getQuestion.likeCount - 1 }, { where: { qId } });
+      await Question.update(
+        { likeCount: getQuestion.likeCount - 1 },
+        { where: { qId } }
+      );
     }
 
     res.send({ data: getQuestion, qResult: resultLike });
