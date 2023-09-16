dbaa76d4e312d1c6865da8c30f8a765920791b7e
정채림 <119480528+ellin45@users.noreply.github.com>
2023-09-16 16:41:05 +0900

    "profile.js수정중"

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,134 +1,126 @@
-const { likeQuestion } = require("../../controller/Cquestion");
-
-function change(type) {
-  const content = document.getElementById("content");
-
-  //처음에 초기화
-  content.innerHTML = "";
-
-  fetchData(type);
-}
 
 function change(buttonType) {
+  console.log(buttonType)
   const contentDiv = document.getElementById('content');
-
+  
   // 서버에서 데이터 가져오기
-  axios.post('/users/profile')
-      .then(response => {
-          const data = response.data;
-
-          switch (buttonType) {
-              case 'likedQuestion':
-                  let likedQuestionsHtml = '';
-                  for (let question of data.likeQuestionData) {
-                      likedQuestionsHtml += `
-                          <div class="question">
-                              <h3>${question.title}</h3>
-                              <p>질문 유형: ${question.qType}</p>
-                              <p>${question.content}</p>
-                              <p>조회수: ${question.viewCount}</p>
-                              <p>좋아요 수: ${question.likeCount}</p>
-                              <p>생성일: ${question.createdAt}</p>
-                              <p>수정일: ${question.updatedAt}</p>
-                          </div>
-                      `;
-                  }
-                  contentDiv.innerHTML = likedQuestionsHtml;
-                  break;
-
-              case 'likedAnswer':
-                  let likedAnswersHtml = '';
-                  for (let answer of data.likeAnswerData) {
-                      likedAnswersHtml += `
-                          <div class="answer">
-                              <h3>${answer.title}</h3>
-                              <p>${answer.content}</p>
-                              <p>좋아요 수: ${answer.likeCount}</p>
-                              <p>생성일: ${answer.createdAt}</p>
-                              <p>수정일: ${answer.updatedAt}</p>
-                          </div>
-                      `;
-                  }
-                  contentDiv.innerHTML = likedAnswersHtml;
-                  break;
-
-              case 'postedQuestion':
-                  let postedQuestionsHtml = '';
-                  for (let post of data.postData) {
-                      postedQuestionsHtml += `
-                          <div class="postedQuestion">
-                              <h3>${post.title}</h3>
-                              <p>질문 유형: ${post.qType}</p>
-                              <p>${post.content}</p>
-                              <p>조회수: ${post.viewCount}</p>
-                              <p>좋아요 수: ${post.likeCount}</p>
-                              <p>생성일: ${post.createdAt}</p>
-                              <p>수정일: ${post.updatedAt}</p>
-                          </div>
-                      `;
-                  }
-                  contentDiv.innerHTML = postedQuestionsHtml;
-                  break;
-
-              case 'postedAnswer':
-                  let postedAnswersHtml = '';
-                  for (let answer of data.answerData) {
-                      postedAnswersHtml += `
-                          <div class="postedAnswer">
-                              <h3>${answer.title}</h3>
-                              <p>${answer.content}</p>
-                              <p>좋아요 수: ${answer.likeCount}</p>
-                              <p>생성일: ${answer.createdAt}</p>
-                              <p>수정일: ${answer.updatedAt}</p>
-                          </div>
-                      `;
-                  }
-                  contentDiv.innerHTML = postedAnswersHtml;
-                  break;
-
-              case 'commented':
-                  let commentHtml = '';
-                  for (let comment of data.commentData) {
-                      commentHtml += `
-                          <div class="comment">
-                              <h3>댓글 ID: ${comment.cId}</h3>
-                              <p>질문 ID: ${comment.qId}</p>
-                              <p>답변 ID: ${comment.aId}</p>
-                              <p>사용자 ID: ${comment.uId}</p>
-                              <p>${comment.content}</p>
-                          </div>
-                      `;
-                  }
-                  contentDiv.innerHTML = commentHtml;
-                  break;
-
-              default:
-                  contentDiv.innerHTML = '선택된 내용이 없습니다.';
+  console.log(data);
+  axios({
+    method:'GET',
+    url: '/users/profile',
+    data:data,
+  }).then(response => {
+    const data = response.data;
+    console.log("Button clicked with type:", buttonType);
+    switch (buttonType) {
+      case 'liked':
+        let likedQuestionsHtml = '';
+        for (let question of data.likeQuestionData) {
+          likedQuestionsHtml += [`
+          <div class="question">
+          <h3>${question.title}</h3>
+          <p>질문 유형: ${question.qType}</p>
+          <p>${question.content}</p>
+          </div>
+          `].join('');
+        }
+        contentDiv.innerHTML = likedQuestionsHtml;
+        break;
+        
+        case 'commented':
+          let likedAnswersHtml = '';
+          for (let answer of data.likeAnswerData) {
+            likedAnswersHtml += [`
+            <div class="answer">
+            <h3>${answer.title}</h3>
+            <p>${answer.content}</p>
+            </div>
+            `].join('');
           }
-      })
-      .catch(error => {
-          console.error('Error fetching data:', error);
-          contentDiv.innerHTML = '데이터를 가져오는데 오류가 발생했습니다.';
-      });
-}
+          contentDiv.innerHTML = likedAnswersHtml;
+          break;
+          
+          case 'qna':
+            let postedQuestionsHtml = '';
+            for (let post of data.postData) {
+              postedQuestionsHtml += [`
+              <div class="postedQuestion">
+              <h3>${post.title}</h3>
+              <p>질문 유형: ${post.qType}</p>
+              <p>${post.content}</p>
+              </div>
+              `].join('');
+            }
+            contentDiv.innerHTML = postedQuestionsHtml;
+            break;
+            
+            case 'answered':
+              let postedAnswersHtml = '';
+              for (let answer of data.answerData) {
+                postedAnswersHtml += [`
+                <div class="postedAnswer">
+                <h3>${answer.title}</h3>
+                <p>${answer.content}</p>
+                </div>
+                `].join('');
+              }
+              contentDiv.innerHTML = postedAnswersHtml;
+              break;
+              
+              case 'commented':
+                let commentHtml = '';
+                for (let comment of data.commentData) {
+                  commentHtml += [`
+                  <div class="comment">
+                  <p>${comment.content}</p>
+                  </div>
+                  `].join('');
+                }
+                contentDiv.innerHTML = commentHtml;
+                break;
+                
+                default:
+                  contentDiv.innerHTML = '선택된 내용이 없습니다.';
+                }
+              })
+              .catch(error => {
+                console.error('Error fetching data:', error);
+                contentDiv.innerHTML = '데이터를 가져오는데 오류가 발생했습니다.';
+              });
+            }
+            
+            document.querySelector(".qna").addEventListener("click", () => {
+              change("likedQuestion");
+              console.log('ㅇㄹㅇㄹ');
+            });
+            document.querySelector(".free").addEventListener("click", () => {
+              change("likedQuestion");
+              console.log('ㅇㄹㅇㄹ');
+
+            });
+          
+            document.querySelector(".liked").addEventListener("click", () => {
+              change("likedQuestion");
+              console.log('ㅇㄹㅇㄹ');
+
+            });
+            
+            document.querySelector(".writed").addEventListener("click", () => {
+              change("postedQuestion");
+              console.log('ㅇㄹㅇㄹ');
+
+            });
+            
+            document.querySelector(".answered").addEventListener("click", () => {
+              change("postedAnswer");
+              console.log('ㅇㄹㅇㄹ');
+
+            });
 
 
-// 이벤트 리스너들
-document.querySelector(".liked").addEventListener("click", () => {
-  displayPosts("liked");
-});
-
-document.querySelector(".writed").addEventListener("click", () => {
-  displayPosts("writed");
-});
-
-document.querySelector(".answered").addEventListener("click", () => {
-  displayPosts("answered");
-});
-
-function fileUpload() {
-  console.log("동적 파일 업로드");
-  // js파일만으로 폼을 전송 ( 파일 데이터를 서버로 전송해야 하는 케이스)
+  function fileUpload() {
+    console.log("동적 파일 업로드");
+    // js파일만으로 폼을 전송 ( 파일 데이터를 서버로 전송해야 하는 케이스)
   // FormData 객체를 활용하면 쉽게 전송 가능!
   const formData = new FormData();
   const file = document.querySelector("#dynamic-file");
