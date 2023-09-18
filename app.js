3b3d2a58ea30295995bd088c243a3563b6170a3e
Jinnymoon <jinnymoon1124@gmail.com>
2023-09-18 15:29:06 +0900

    refactor: 헤더, 회원정보, 수정 페이지 수정

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.get('*', (req, res) => {
   res.render('404.ejs');
 });
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => {
     console.log(`server open on port ${PORT}`);
   });
