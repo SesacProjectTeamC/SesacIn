7e8a72fbc2cdc672393c8c9cf565c23075ad1595
정채림 <chaelimj45@gmail.com>
2023-09-18 17:51:51 +0900

    "profile.ejs, favicon 추가"

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,69 +1,101 @@
 function change(buttonType) {
-    console.log(buttonType);
-    const contentDiv = document.getElementById("content");
-
-    axios({
-        method: "GET",
-        url: `/users/profile/yes`,
-    })
+  console.log(buttonType);
+  const contentDiv = document.getElementById("content");
+  const cautionA = document.getElementsByClassName("caution");
+  axios({
+    method: "GET",
+    url: `/users/profile/yes`,
+  })
     .then((response) => {
-        const data = response.data;
-        if (buttonType === 'liked') {
-            liked(data, contentDiv);
-            return;
-        } else if (buttonType === 'commented') {
-            commented(data, contentDiv);
-            return;
-        } else if (buttonType === 'answered') {
-            answered(data, contentDiv);
-            return;
-        } else if (buttonType === 'qna') {
-            qna(data, contentDiv);
-            return;
-        } else if (buttonType === 'free') {
-            free(data, contentDiv);
-            return;
-        } else {
-            contentDiv.innerHTML = "선택된 내용이 없습니다.";
-        }
+      const data = response.data;
+      if (buttonType === "liked") {
+        liked(data, contentDiv);
+        return;
+      } else if (buttonType === "commented") {
+        commented(data, contentDiv);
+        return;
+      } else if (buttonType === "answered") {
+        answered(data, contentDiv);
+        return;
+      } else if (buttonType === "qna") {
+        qna(data, contentDiv);
+        return;
+      } else if (buttonType === "free") {
+        free(data, contentDiv);
+        return;
+      } else {
+        contentDiv.innerHTML = "선택된 내용이 없습니다.";
+      }
     })
     .catch((error) => {
-        console.error("Error fetching data:", error);
-        contentDiv.innerHTML = "데이터를 가져오는데 오류가 발생했습니다.";
+      console.error("Error fetching data:", error);
+      cautionA.innerHTML = "작성하신 글이 없습니다.";
     });
 }
 const liked = (data, contentDiv) => {
-    for (let question of data.likeQuestionData) {
-        contentDiv.innerHTML = [
-            
-            `<div class="question">
+  contentDiv.innerHTML = "";
+  for (let question of data.likeQuestionData) {
+    contentDiv.innerHTML = [
+      `<div class="question">
             <a href ="/board/detail/${data.qId}">
             <h3>${question.title}</h3>
             <p>질문 유형: ${question.qType}</p>
             <p>${question.content}</p>
-            </div>`
-        ]
-        ;
-    }
-    console.log('liked');
-}
+            </div>`,
+    ];
+  }
+  console.log("liked");
+
+  for (let boards of data.boards) {
+    contentDiv.innerHTML = [
+      `
+                <div class="freeBoards">
+                <p>${boards.title}</p>
+                <p>${boards.content}</p>
+                <div class="like">
+                <img src="../../static/svg/heart.svg" alt="좋아요" width="5px" class="svg"/>
+                <p>${boards.likeCount}</p>
+                <img src="../../static/svg/message.svg" alt="답변개수" width="5px" class="svg"/>
+                <p>${boards.viewCount}</p>
+                </div>`,
+    ];
+  }
+  
+};
 const commented = (data, contentDiv) => {
-    console.log(data);
-    for (let answer of data.likeAnswerData) {
-        contentDiv.innerHTML = [`
+  console.log(data);
+  contentDiv.innerHTML = "";
+  for (let answer of data.likeAnswerData) {
+    contentDiv.innerHTML = [
+      `
         <a href ="/board/detail/${data.cId}">
         <div class="answer">
         <h3>${answer.title}</h3>
         <p>${answer.content}</p>
-        </div>`
-    ]
-    ;
-}
-console.log('commented');
-}
+        </div>`,
+    ];
+  }
+  console.log("commented");
+  for (let boards of data.boards) {
+    contentDiv.innerHTML = [
+      `
+                <div class="freeBoards">
+                <p>${boards.title}</p>
+                <p>${boards.content}</p>
+                <div class="like">
+                <img src="../../static/svg/heart.svg" alt="좋아요" width="5px" class="svg"/>
+                <p>${boards.likeCount}</p>
+                <img src="../../static/svg/message.svg" alt="답변개수" width="5px" class="svg"/>
+                <p>${boards.viewCount}</p>
+                </div>`,
+    ];
+  }
+};
 const qna = (data, contentDiv) => {
-    for (let post of data.postData) {
-        contentDiv.innerHTML = [`
+  contentDiv.innerHTML = "";
+  for (let post of data.postData) {
+    contentDiv.innerHTML = [
+      `
         <a href ="/board/detail/${data.qId}">
         <div class="postedQuestion">
         <h3>${post.title}</h3>
@@ -73,27 +105,30 @@ const qna = (data, contentDiv) => {
         <p>${post.likeCount}</p>
         <img src="../../static/svg/message.svg" alt="답변개수" width="5px" class="svg"/>
         <p>${post.viewCount}</p>
-        </div>`
-    ]
-}
-console.log('qna');
-}
+        </div>`,
+    ];
+  }
+  console.log("qna");
+};
 const answered = (data, contentDiv) => {
-    for(let answer of data.answerData){
-        contentDiv.innerHTML = [
-            `<a href ="/board/detail/${data.aId}">
+  contentDiv.innerHTML = "";
+  for (let answer of data.answerData) {
+    contentDiv.innerHTML = [
+      `<a href ="/board/detail/${data.aId}">
             <div class="postedAnswer">
             <h3>${answer.title}</h3>
             <p>${answer.content}</p>
             </div>
-            `
-        ]   
-    }
-    console.log('answered');
-        }
-const free = (data, contentDiv) =>{
-            for(let boards of data.boards){
-                contentDiv.innerHTML = [`
+            `,
+    ];
+  }
+  console.log("answered");
+};
+const free = (data, contentDiv) => {
+    contentDiv.innerHTML = "";
+  for (let boards of data.boards) {
+    contentDiv.innerHTML = [
+      `
                 <div class="freeBoards">
                 <p>${boards.title}</p>
                 <p>${boards.content}</p>
@@ -102,31 +137,32 @@ const free = (data, contentDiv) =>{
                 <p>${boards.likeCount}</p>
                 <img src="../../static/svg/message.svg" alt="답변개수" width="5px" class="svg"/>
                 <p>${boards.viewCount}</p>
-                </div>`
-                ,
-            ]
+                </div>`,
+    ];
+  }
+  console.log("free");
+};
+
+function isSesac(data) {
+  const sesacElements = document.getElementsByClassName("sesac_badge");
+  axios({
+    method: "GET",
+    url: "/users/profile",
+  })
+    .then((response) => {
+      const data = response.data;
+      if (data.isSesac === true) {
+        for (let element of sesacElements) {
+          element.style.display = ""; // 기본값으로 재설정하여 엘리먼트를 보이게 합니다.
         }
-        console.log('free');
-    }
-    
-function isSesac(data){
-    const sesacElements = document.getElementsByClassName('sesac_badge');
-    axios({
-        method: "GET",
-        url: '/users/profile',
-    }) .then((response) => {
-        const data = response.data
-        if(data.isSesac === true){
-            for (let element of sesacElements) {
-                element.style.display = '';  // 기본값으로 재설정하여 엘리먼트를 보이게 합니다.
-            }
-        } else {
-            for (let element of sesacElements) {
-                element.style.display = 'none';  // 엘리먼트를 숨깁니다.
-            }
+      } else {
+        for (let element of sesacElements) {
+          element.style.display = "none"; // 엘리먼트를 숨깁니다.
         }
-    }).catch((error) => {
-        console.error("Error fetching data:", error);
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
     });
 }
 
@@ -151,8 +187,12 @@ function fileUpload() {
       "Content-Type": "multipart/form-data",
     },
   }).then((res) => {
-    const { data } = res;
+    const {data} = res;
     console.log(data);
     document.querySelector("img").src = "/" + data.path;
   });
+}
+
+function goTohome() {
+  window.location.href = "/";
 }
