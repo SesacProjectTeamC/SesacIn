bb056b911d2fba81d0f0fe5216df7aa8064aea36
hotdog7778 <rlaxorbs159@gmail.com>
2023-09-19 18:24:51 +0900

    refactor: 메인페이지/자유게시판 상세 페이지

--- controller/Cmain.js
+++ controller/Cmain.js
@@ -1,4 +1,4 @@
-const { Question, Board, Answer, Comment, uLike, User } = require('../models');
+const { Question, Board, Answer, Comment, uLike, User, sequelize } = require('../models');
 const { Op } = require('sequelize');
 const moment = require('moment');
 
@@ -14,106 +14,78 @@ exports.getMainPage = async (req, res) => {
     // 페이지 설정
     let page = parseInt(req.params.page) || 1;
     let pageSize = parseInt(req.params.pageSize) || 20;
+    let offset = (page - 1) * pageSize;
+    let sortField = req.params.sortField || 'createdAt';
+    let sortOrder = req.params.sortOrder || 'desc';
+
+    // 페이지 수 (올림처리)
     const questionTotalCount = await Question.count();
-    const questionPageCount = parseInt(
-      Math.ceil(questionTotalCount / pageSize)
-    ); // 페이지 수 (올림처리)
+    const questionPageCount = parseInt(Math.ceil(questionTotalCount / pageSize));
     const boardTotalCount = await Board.count();
     const boardPageCount = parseInt(Math.ceil(boardTotalCount / pageSize)); // 페이지 수 (올림처리)
 
-    // 페이지별 Question 데이터 조회
-    const paginatedQuestion = await Question.findAll({
-      order: [['createdAt', 'DESC']], // 정렬 기준
-      limit: pageSize,
-      offset: (page - 1) * pageSize,
-    });
+    // 데이터 조회
+    // 1. 질문
+    const questionSql = `
+    SELECT q.qId, u.uName, u.uId, u.userImgPath, q.title, q.content, q.viewCount, q.likeCount, q.createdAt, q.updatedAt, COALESCE(count(a.aId), 0) as answerCount
+      FROM question q
+      LEFT JOIN answer a ON q.qId = a.qId
+      LEFT JOIN user u ON q.uId = u.uId
+      GROUP BY q.qId
+      ORDER BY ${sortField} ${sortOrder} 
+      LIMIT ${offset}, ${pageSize};`;
+    const [paginatedQuestion] = await sequelize.query(questionSql);
+    // 2. 자유
+    const boardSql = `
+    SELECT b.bId, u.uName, u.uId, u.userImgPath, b.title, b.content, b.viewCount, b.likeCount, b.createdAt, b.updatedAt, COALESCE(count(c.cId), 0) as commentCount 
+      FROM board b 
+      LEFT JOIN comment c ON b.bId = c.bId 
+      LEFT JOIN user u ON b.uId = u.uId 
+      GROUP BY b.bId 
+      ORDER BY ${sortField} ${sortOrder} 
+      LIMIT ${offset}, ${pageSize};`;
+    const [paginatedBoard] = await sequelize.query(boardSql);
 
-    // 페이지별 Board 호출
-    const paginatedBoard = await Board.findAll({
-      order: [['createdAt', 'DESC']], // 정렬 기준
-      limit: pageSize,
-      offset: (page - 1) * pageSize,
-    });
-
-    // QnA & Board. createdAt 포맷 변경 후 배열에 저장
+    // 날짜데이터 포맷 변경
+    // 1. 질문
     const questionCreateAt = [];
     for (q of paginatedQuestion) {
-      questionCreateAt.push(
-        moment(q.dataValues.createdAt).format('YYYY-MM-DD')
-      );
+      questionCreateAt.push(moment(q.createdAt).format('YYYY-MM-DD'));
     }
+    // 2. 답변
+    // 날짜 데이터 포맷 변경
     const boardCreateAt = [];
     for (b of paginatedBoard) {
-      boardCreateAt.push(moment(b.dataValues.createdAt).format('YYYY-MM-DD'));
+      boardCreateAt.push(moment(b.createdAt).format('YYYY-MM-DD'));
     }
 
-    // QnA & Board. uNname 배열에 저장
-    const questionUserName = [];
-    for (q of paginatedQuestion) {
-      // User 모델로 uid가지고 uName 가져오기
-      const user = await User.findByPk(q.uId);
-      questionUserName.push(user.uName);
-    }
-    const boardUserName = [];
-    for (b of paginatedBoard) {
-      // User 모델로 uid가지고 uName 가져오기
-      const user = await User.findByPk(b.uId);
-      boardUserName.push(user.uName);
-    }
-
-    // QnA & Board. Comment 배열에 저장 후 총 갯수 계산
-    const questionCommentCount = [];
-    for (q of paginatedQuestion) {
-      // Comment 모델로 qid가지고 count 세기
-      const count = await Comment.count({
-        where: {
-          qId: q.qId,
-        },
-      });
-      questionCommentCount.push(count);
-    }
-    const boardCommentCount = [];
-    for (b of paginatedBoard) {
-      // Comment 모델로 bid가지고 count 세기
-      const count = await Comment.count({
-        where: {
-          bId: b.bId,
-        },
-      });
-      boardCommentCount.push(count);
-    }
     if (isLogin) {
       const uId = req.session.user;
 
       const user = await User.findOne({
         where: { uId },
       });
-      // res.status(200).render('mainTest', {
+
       res.status(200).render('main', {
         questionData: paginatedQuestion, // question 데이터(20개씩)
-        questionCreateAt, // question 데이터에서 CreateAt의 포맷팅을 변경한 데이터
-        questionUserName, // question 데이터에서 uname을 가져와서
-        questionCommentCount, // question 데이터에서 CommentCount을 가져와서
-        boardData: paginatedBoard, // Board 데이터(20개씩)
-        boardCreateAt, // Board 데이터에서 CreateAt의 포맷팅을 변경한 데이터
-        boardUserName, // Board 데이터에서 uname을 가져와서
-        boardCommentCount, // Board 데이터에서 CommentCount을 가져와서
+        questionCreateAt,
+        questionPageCount,
+        boardData: paginatedBoard,
+        boardCreateAt,
+        boardPageCount,
         isLogin,
         currentUserId,
         success: true,
         userData: user,
       });
     } else {
-      // res.status(200).render('mainTest', {
       res.status(200).render('main', {
         questionData: paginatedQuestion, // question 데이터(20개씩)
-        questionCreateAt, // question 데이터에서 CreateAt의 포맷팅을 변경한 데이터
-        questionUserName, // question 데이터에서 uname을 가져와서
-        questionCommentCount, // question 데이터에서 CommentCount을 가져와서
-        boardData: paginatedBoard, // Board 데이터(20개씩)
-        boardCreateAt, // Board 데이터에서 CreateAt의 포맷팅을 변경한 데이터
-        boardUserName, // Board 데이터에서 uname을 가져와서
-        boardCommentCount, // Board 데이터에서 CommentCount을 가져와서
+        questionCreateAt,
+        questionPageCount,
+        boardData: paginatedBoard,
+        boardCreateAt,
+        boardPageCount,
         isLogin,
         currentUserId,
         success: true,
--- controller/Cquestion.js
+++ controller/Cquestion.js
@@ -1,11 +1,4 @@
-const {
-  Question,
-  Answer,
-  Comment,
-  uLike,
-  User,
-  sequelize,
-} = require('../models');
+const { Question, Answer, Comment, uLike, User, sequelize } = require('../models');
 const moment = require('moment');
 
 //=== 메인페이지,질문 목록 가져오기 ===
@@ -23,12 +16,7 @@ exports.getQuestions = async (req, res) => {
     let sortOrder = req.params.sortOrder || 'desc';
 
     // params 검사
-    if (
-      !sortField ||
-      !['createdAt', 'likeCount', 'viewCount', 'answerCount'].includes(
-        sortField
-      )
-    ) {
+    if (!sortField || !['createdAt', 'likeCount', 'viewCount', 'answerCount'].includes(sortField)) {
       res.status(400).send({ error: '올바른 정렬 필드를 지정하세요.' });
       return;
     }
@@ -38,9 +26,7 @@ exports.getQuestions = async (req, res) => {
     }
 
     const questionTotalCount = await Question.count();
-    const questionPageCount = parseInt(
-      Math.ceil(questionTotalCount / pageSize)
-    ); // 페이지 수 (올림처리)
+    const questionPageCount = parseInt(Math.ceil(questionTotalCount / pageSize)); // 페이지 수 (올림처리)
 
     // 시퀄라이즈에 SQL 쿼리 그대로 사용
     // offset부터 ~~ offset+pageSize 만큼의 데이터만 불러온다.
@@ -181,12 +167,7 @@ exports.paginateQuestion = async (req, res) => {
     let sortOrder = req.params.sortOrder || 'desc';
 
     // params 검사
-    if (
-      !sortField ||
-      !['createdAt', 'likeCount', 'viewCount', 'answerCount'].includes(
-        sortField
-      )
-    ) {
+    if (!sortField || !['createdAt', 'likeCount', 'viewCount', 'answerCount'].includes(sortField)) {
       res.status(400).send({ error: '올바른 정렬 필드를 지정하세요.' });
       return;
     }
@@ -196,9 +177,7 @@ exports.paginateQuestion = async (req, res) => {
     }
 
     const questionTotalCount = await Question.count();
-    const questionPageCount = parseInt(
-      Math.ceil(questionTotalCount / pageSize)
-    ); // 페이지 수 (올림처리)
+    const questionPageCount = parseInt(Math.ceil(questionTotalCount / pageSize)); // 페이지 수 (올림처리)
 
     // 시퀄라이즈에 SQL 쿼리 그대로 사용
     // offset부터 ~~ offset+pageSize 만큼의 데이터만 불러온다.
@@ -584,10 +563,7 @@ exports.likeQuestion = async (req, res) => {
         });
 
         // (2) 질문 likeCount 업데이트 +1
-        await Question.update(
-          { likeCount: getQuestion.likeCount + 1 },
-          { where: { qId } }
-        );
+        await Question.update({ likeCount: getQuestion.likeCount + 1 }, { where: { qId } });
 
         console.log('성공 !!');
 
@@ -600,10 +576,7 @@ exports.likeQuestion = async (req, res) => {
         });
 
         // (2) 질문 likeCount 업데이트 -1
-        await Question.update(
-          { likeCount: getQuestion.likeCount - 1 },
-          { where: { qId } }
-        );
+        await Question.update({ likeCount: getQuestion.likeCount - 1 }, { where: { qId } });
 
         res.send({ data: getQuestion, qResult: false });
       }
