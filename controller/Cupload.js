e513f3bce351a16da00da3c522f2b3c7f3ea2b44
hotdog7778 <rlaxorbs159@gmail.com>
2023-09-21 02:43:01 +0900

    refactor: 에디터 URL변수 동적할당
    description:
     - 서버의 URL 이 변경되더라도 클라이언트의 요청 URL을 동적으로 받아서 처리하도록 코드 변경
     - 결과로 .env의 URL 변수 불필요 해짐

--- controller/Cupload.js
+++ controller/Cupload.js
@@ -54,11 +54,10 @@ exports.uploadEditImageFile = async (req, res) => {
     const { originalname, path } = req.file;
 
     // 파일 업로드 성공
-    const { URL } = process.env;
     res.status(200).send({
       success: true,
       msg: '파일이 성공적으로 업로드되었습니다.',
-      url: `${URL}/${path}`,
+      path,
     });
   } catch (error) {
     console.error('파일 업로드 중 오류 발생:', error);
